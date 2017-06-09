043a3ddf9dc350b3932248b5c7599c9c5585a685
SmallJava <191172213@qq.com>
2017-06-09 19:13:13 +0800

    加载Tomcat

--- servletDemo/src/main/java/cn/bdqn/tangcco/action/LoginAction.java
+++ servletDemo/src/main/java/cn/bdqn/tangcco/action/LoginAction.java
@@ -1,6 +1,9 @@
 package cn.bdqn.tangcco.action;
 
+import cn.bdqn.tangcco.entity.User;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,16 +12,26 @@ import java.io.IOException;
 /**
  * Created by Administrator on 2017/6/9.
  */
+@WebServlet(name = "login", urlPatterns = "/login.action")
 public class LoginAction extends HttpServlet {
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("执行doGet()方法");
-//        super.doGet(req, resp);
+        this.doPost(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("执行doPost()方法");
-//        super.doPost(req, resp);
+        super.doPost(req, resp);
     }
 }
--- servletDemo/src/main/java/cn/bdqn/tangcco/entity/User.java
+++ servletDemo/src/main/java/cn/bdqn/tangcco/entity/User.java
@@ -4,6 +4,19 @@ package cn.bdqn.tangcco.entity;
  * Created by Administrator on 2017/6/9.
  */
 public class User {
-    private String userName;
-    private String passWord;
+    private String userName="admin";
+    private String passWord="1234";
+
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getPassWord() {
+        return passWord;
+    }
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
 }
